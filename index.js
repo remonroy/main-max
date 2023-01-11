ccb14e572a1320b7b4625f549ddd0384ea7f0952
unknown <remonroy34@gmail.com>
2023-01-11 13:46:41 +0600

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-var data = [10, 1000, 1, 59.1, 120, 4, -1, 30];
+var arr = [10, 1000, 1, 59.1, 120, 4, -1, 30];
 
-console.log(Math.max(...data));
+// console.log(Math.max(...data));
 // Output 1000
-console.log(Math.min(...data));
+// console.log(Math.min(...data));
 // Output -1
+
+let max = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] >= max) {
+    max = arr[i];
+    // console.log("Max value = ", max);
+  }
+}
+console.log("Max value = ", max);
+
+let min = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < min) {
+    min = arr[i];
+    // console.log("Max value = ", max);
+  }
+}
+console.log("Min value = ", min);
